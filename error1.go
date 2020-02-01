718331d858cc4c3e26a9d154cb1565c66cc30e09
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-01 16:57:10 +0530

    removed S1

--- error1.go
+++ error1.go
@@ -15,3 +15,19 @@ type errorString struct {
 func (e *errorString) Error() string {
    return e.s
 }
+ 
+func New(text string) error {
+   return &errorString{text}
+}
+
+func main() {
+   if err := webCall(); err != nil {
+      fmt.Println(err)
+      return 
+   }
+   fmt.Println("Life is good")
+}
+
+func webCall() error {
+   return New("BAD Request")
+}
