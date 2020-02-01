2f5ab3529561ab454a143824581c4f7e2c4a58f4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-01 20:30:07 +0530

    removed S1

---
+++ error4.go
@@ -0,0 +1,57 @@
+// Package example4 provides code to show how to implement behavior as context.
+package example4
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+// client represents a single coneection in the room
+type client struct {
+   name string
+   reader *bufio.Reader
+}
+
+// TypeAsContext shows how to check multiple types of possible custom error
+// types that can be returned from the net package
+func (c *client) TypeAsContext() {
+	for {
+		line, err := c.reader.ReadString('\n')
+		if err != nil {
+			switch e := err.(type) {
+			case *net.OpError:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
+
+			case *net.AddrError:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
+
+			case *net.DNSConfigError:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
+
+			default:
+				if err == io.EOF {
+					log.Println("EOF: Client leaving chat")
+					return
+				}
+
+				log.Println("read-routine", err)
+			}
+		}
+
+		fmt.Println(line)
+	}
+}
+
+
