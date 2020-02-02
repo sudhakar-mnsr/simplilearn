1d3d59beb7c8f9f66a3a718394eb790c8d029973
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-02 17:59:18 +0530

    removed S1

--- wrap.go
+++ wrap.go
@@ -16,3 +16,29 @@ type AppError struct {
 func (c *AppError) Error() string {
 	return fmt.Sprintf("App Error, State: %d", c.State)
 }
+
+func main() {
+
+	// Make the function call and validate the error.
+	if err := firstCall(10); err != nil {
+
+		// Use type as context to determine cause.
+		switch v := errors.Cause(err).(type) {
+		case *AppError:
+
+			// We got our custom error type.
+			fmt.Println("Custom App Error:", v.State)
+
+		default:
+
+			// We did not get any specific error type.
+			fmt.Println("Default Error")
+		}
+
+		// Display the stack trace for the error.
+		fmt.Println("\nStack Trace\n********************************")
+		fmt.Printf("%+v\n", err)
+		fmt.Println("\nNo Trace\n********************************")
+		fmt.Printf("%v\n", err)
+	}
+}
