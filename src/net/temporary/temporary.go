5f38ac9dd80f735094fdfff599257d86409a7230
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-03 23:29:53 +0530

    examples

--- src/net/temporary/temporary.go
+++ src/net/temporary/temporary.go
@@ -16,3 +16,21 @@ type AppError struct {
 func (c *AppError) Error() string {
 	return fmt.Sprintf("App Error, State: %d", c.State)
 }
+type temporary interface {
+	Temporary() bool
+}
+
+func (e *OpError) Temporary() bool {
+	// Treat ECONNRESET and ECONNABORTED as temporary errors when
+	// they come from calling accept. See issue 6163.
+	if e.Op == "accept" && isConnError(e.Err) {
+		return true
+	}
+
+	if ne, ok := e.Err.(*os.SyscallError); ok {
+		t, ok := ne.Err.(temporary)
+		return ok && t.Temporary()
+	}
+	t, ok := e.Err.(temporary)
+	return ok && t.Temporary()
+}
