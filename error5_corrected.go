c776d30573b6310a5acc74f34c24fd9b3cfa642c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-03 23:11:55 +0530

    examples

--- error5_corrected.go
+++ error5_corrected.go
@@ -13,3 +13,7 @@ type customError struct{}
 func (c *customError) Error() string {
 	return "Find the bug."
 }
+// fail returns nil values for both return types.
+func fail() ([]byte, *customError) {
+	return nil, nil
+}
