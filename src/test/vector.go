dc7ef2e8ffd81837ab689b353c15832c5db2a2d5
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-04 20:16:23 +0530

    examples

--- src/test/vector.go
+++ src/test/vector.go
@@ -33,3 +33,17 @@ func (v SimpleVector) assertLenMatch(other Vector) {
       panic("Vector length mismatch")
    }
 }
+
+// String returns a string representation of the Vector
+func (v SimpleVector) String() string {
+   buff := bytes.NewBufferString("[")
+   for i, val := range v {
+      buff.WriteString(srconv.FormatFloat(val, 'g', -1, 64))
+      if i < len(v)-1 {
+         buff.WriteRune(',')
+      }
+   }
+   buff.WriteRune(']')
+   return buff.String()
+}
+
