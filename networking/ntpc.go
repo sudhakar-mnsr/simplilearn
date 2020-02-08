e78930eb504fdd5798aa556ed3866a91efa893cb
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-08 18:03:23 +0530

    examples

--- networking/ntpc.go
+++ networking/ntpc.go
@@ -75,3 +75,21 @@ func main() {
 		os.Exit(1)
 	}
 
+	// NTP data comes in as big-endian (LSB [0...47] MSB)
+	// with a 64-bit value containing the server time in seconds
+	// where the first 32-bits are seconds and last 32-bit are fractional.
+	// The following extracts the seconds from [0...[40:43]...47]
+	// it is the number of secs since 1900 (NTP epoch)
+	secs := binary.BigEndian.Uint32(rsp[40:])
+	frac := binary.BigEndian.Uint32(rsp[44:])
+
+	// Many OSs use Unix time epoch which is num of secs since 1970,
+	// while NTP's ephoch starts on Jan 1, 1900.  Therefore,
+	// to get the correct time, we must adjust the epocs properly
+	// by removing 70 yrs of seconds (1970-1900) offset.
+	ntpEpoch := time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
+	unixEpoch := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	offset := unixEpoch.Sub(ntpEpoch).Seconds()
+	now := float64(secs) - offset
+	fmt.Printf("%v\n", time.Unix(int64(now), int64(frac)))
+}
