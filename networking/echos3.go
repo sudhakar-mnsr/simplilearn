49507241814f923ae441cfdf680fa9f45b188365
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-09 19:42:51 +0530

    examples

---
+++ networking/echos3.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+// This program implements a simple echo server over that is able
+// to use TCP or Unix Domain Socket (streaming).
+// When the server receives a request, it returns its content immediately.
+//
+// Usage:
+// echos2
+//   -e <endpoint: ip addr or path>
+//   - n <protoco [tcp,unix]>
+func main() {
+	var addr string
+	var network string
+	flag.StringVar(&addr, "e", ":4040", "service endpoint [ip addr or socket path]")
+	flag.StringVar(&network, "n", "tcp", "network protocol [tcp,unix]")
+	flag.Parse()
+
+	// validate network
+	switch network {
+	case "tcp", "tcp4", "tcp6", "unix":
+	default:
+		fmt.Println("unsupported network protocol")
+		os.Exit(1)
+	}
+
+	// announce service using the Listen function
+	// which creates a generic Listen listener.
+	l, err := net.Listen(network, addr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Printf("listening at (%s) %s\n", network, addr)
+
+	// req/response loop
+	for {
+		// use Listener to block and wait for connection
+		// request using function Accept() which then
+		// creates a generic Conn value.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("failed to accept conn:", err)
+			conn.Close()
+			continue
+		}
+		fmt.Println("connected to: ", conn.RemoteAddr())
+
+		go handleConnection(conn)
+	}
+}
