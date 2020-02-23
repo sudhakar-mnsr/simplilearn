b30c902a19885ab29e79d1cb6f2c16674344b41a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-23 14:01:36 +0530

    examples

---
+++ gob0.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+   "encoding/gob"
+   "fmt"
+   "os"
+   "time"
+)
+
+type Name struct {
+   First, Last string
+}
+
+type Book struct {
+   Title string
+   PageCount int
+   ISBN string
+   Authors []Name
+   Publisher string
+   PublishDate time.Time
+}
+
+
