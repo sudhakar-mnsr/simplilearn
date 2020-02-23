86668a76f4187b6d162835f2728e4ba923ee4cf8
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-23 14:12:32 +0530

    examples

--- gob0.go
+++ gob0.go
@@ -21,28 +21,40 @@ type Book struct {
 }
 
 func main() {
-books := []Book{
-   Book{
-      Title: "Learning Go",
-      PageCount: 375,
-      ISBN: "123"
-      Authors: []Name{{"Vladimir", "Vivien"},{"MNSR", "Sudhakar"},},
-      Publisher: "Packt",
-      PublishDate: time.Date(2016, time.July, 0, 0, 0, 0, 0, time.UTC),
-   },
-   Book{
-      Title: "The Go Programing Language",
-      PageCount: 380,
-      ISBN: "456"
-      Authors: []Name{{"Brian", "Kernighan"},{"MNSR", "Sudhakar"},{"Alan", "Donavan"}},
-      Publisher: "Addison-Wesley",
-      PublishDate: time.Date(2015, time.October, 26, 0, 0, 0, 0, time.UTC),
-   },
-   Book{
-      Title: "Introducing Go",
-      PageCount: 124,
-      ISBN: "8910"
-      Authors: []Name{{"Caleb", "Doxsey"},{"MNSR", "Sudhakar"}},
-      Publisher: "OReilly",
-      PublishDate: time.Date(2016, time.January, 26, 0, 0, 0, 0, time.UTC),
-   },
+   books := []Book{
+      Book{
+         Title: "Learning Go",
+         PageCount: 375,
+         ISBN: "123"
+         Authors: []Name{{"Vladimir", "Vivien"},{"MNSR", "Sudhakar"},},
+         Publisher: "Packt",
+         PublishDate: time.Date(2016, time.July, 0, 0, 0, 0, 0, time.UTC),
+      },
+      Book{
+         Title: "The Go Programing Language",
+         PageCount: 380,
+         ISBN: "456"
+         Authors: []Name{{"Brian", "Kernighan"},{"MNSR", "Sudhakar"},{"Alan", "Donavan"}},
+         Publisher: "Addison-Wesley",
+         PublishDate: time.Date(2015, time.October, 26, 0, 0, 0, 0, time.UTC),
+      },
+      Book{
+         Title: "Introducing Go",
+         PageCount: 124,
+         ISBN: "8910"
+         Authors: []Name{{"Caleb", "Doxsey"},{"MNSR", "Sudhakar"}},
+         Publisher: "OReilly",
+         PublishDate: time.Date(2016, time.January, 26, 0, 0, 0, 0, time.UTC),
+      },
+   
+   file, err := os.Create("book.dat")
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   
+   enc := gob.NewEncoder(file)
+   if err := enc.Encode(books); err != nil {
+      fmt.Println(err)
+   }
+}
