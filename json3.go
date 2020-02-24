823f51e134aa8905ad797ff5a9dacad07226eeb6
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-24 15:09:43 +0530

    examples

---
+++ json3.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+   "encoding/json"
+   "fmt"
+   "os"
+   "time"
+)
+
+type Name struct {
+   First, Last string
+}
+
+func (n *Name) MarshalJSON() ([]byte, error) {
+   return []byte(fmt.Sprintf("\"%s, %s\"", n.Last, n.First)), nil
+}
+
