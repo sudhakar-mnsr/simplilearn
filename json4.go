5c3b7fbee52d3149cd54afdf9e06f5482c1682e8
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-24 15:19:54 +0530

    examples

--- json4.go
+++ json4.go
@@ -11,3 +11,15 @@ import (
 type Name struct {
 	First, Last string
 }
+
+func (n *name) UnmarshalJSON(data []byte) error {
+   var name string
+   err := json.Unmarshal(data, &name)
+   if err != nil
+      fmt.Println(err)
+      return err
+   }
+   parts := strings.Split(name, ", ")
+   n.Last, n.First = parts[0], parts[1]
+   return nil
+}
