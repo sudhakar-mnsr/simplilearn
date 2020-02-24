5a57dba0e0ed565ffdd69cf965c4807f2286838a
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-24 15:10:19 +0530

    examples

--- json3.go
+++ json3.go
@@ -15,3 +15,11 @@ func (n *Name) MarshalJSON() ([]byte, error) {
    return []byte(fmt.Sprintf("\"%s, %s\"", n.Last, n.First)), nil
 }
 
+type Book struct {
+	Title       string
+	PageCount   int
+	ISBN        string
+	Authors     []Name
+	Publisher   string
+	PublishDate time.Time
+}
