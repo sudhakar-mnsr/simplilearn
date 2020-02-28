80b142098f5fd0c794a28821577133190a710616
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-02-28 13:27:47 +0530

    examples

--- mutex6.go
+++ mutex6.go
@@ -11,29 +11,36 @@ const MAX = 1000
 const workers = 2
 
 func main() {
-values := make(chan int)
-result := make(chan int, workers)
-var wg sync.WaitGroup
-
-go func() {
-   for i := 1; i < MAX; i++ {
-      if (i%3) == 0 || (i%5) == 0 {
-         values <- i
-      }
-   }
-   close(values)
-}()
+   values := make(chan int)
+   result := make(chan int, workers)
+   var wg sync.WaitGroup
    
-work := func() {
-   defer wg.Done()
-   r := 0
-   for i := range values {
-      r += i
+   go func() {
+      for i := 1; i < MAX; i++ {
+         if (i%3) == 0 || (i%5) == 0 {
+            values <- i
+         }
+      }
+      close(values)
+   }()
+      
+   work := func() {
+      defer wg.Done()
+      r := 0
+      for i := range values {
+         r += i
+      }
+      result <- r
    }
-   result <- r
-}
-
-wg.Add(workers)
-for i := 0; i < workers; i++ {
-   go work()
+   
+   wg.Add(workers)
+   for i := 0; i < workers; i++ {
+      go work()
+   }
+   
+   wg.Wait()
+   close(result)
+   total := 0
+   total = <-result + <-result
+   fmt.Println("Total:", total)
 }
