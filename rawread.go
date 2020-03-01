67caa75113bc21200905a9385bcda4dac329830c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-01 14:16:29 +0530

    examples

---
+++ rawread.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+   "fmt"
+   "io"
+   "os"
+)
+
+func main) {
+   fin, err := os.Open("./dict.txt")
+   if err != nil {
+      fmt.Println(err)
+   }
+   defer fin.Close()
+   p := make([]byte, 1024)
+   for {
+      n, err := fin.Read(p)
+      if err == io.EOF {
+         break
+      }
+      fmt.Print(string(p[:n]))
+   }
+}
