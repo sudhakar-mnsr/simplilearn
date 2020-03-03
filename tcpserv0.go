6d6f6e1bd5648429edfef1a808bd349ae8588ef1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-03 14:50:42 +0530

    examples

---
+++ tcpserv0.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+   "fmt"
+   "net"
+   "os"
+   "strings"
+   "time"
+   curr "curr0"
+)
+
+var currencies = curr.Load("./data.csv")
+
+func main() {
+   ln, err := net.Listen("tcp", ":4040")
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+   defer ln.Close()
+   fmt.Println("Global Currency Service... Listening on port 4040")
+
+   // Connection-loop - handle incoming requests
+   for {
+      conn, err := ln.Accept()
+      if err != nil {
+         fmt.Println(err)
+         conn.Close()
+         continue
+      }
+      fmt.Println("Connected to ", conn.RemoteAddr())
+      go handleConnection(conn)
+   }
+}
+
