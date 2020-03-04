b3606ac8a535b71decb161391b953af1a8a33375
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-04 21:25:46 +0530

    examples

---
+++ httpserver6.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+   "fmt"
+   "net/http"
+)
+
+func main() {
+   hello := func(resp http.ResponseWriter, req *http.Request) {
+      resp.Header().Add("Content-Type", "text/html")
+      resp.WriteHeader(http.StatusOK)
+      fmt.Fprint(resp, "Hello from Above!")
+   }
+   goodbye := func(resp http.ResponseWriter, req *http.Request) {
+      resp.Header().Add("Content-Type", "text/html")
+      resp.WriteHeader(http.StatusOK)
+      fmt.Fprint(resp, "Goodbye, its been real!")
+   }
+   http.HandleFunc("/hello", hello)
+   http.HandleFunc("/goodbye", goodbye)
+
+   http.ListenAndServe(":4040", nil)
+}
+  
