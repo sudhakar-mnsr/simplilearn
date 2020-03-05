251d243d9cf6c4cbab97df6839f97490fa89668c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-05 12:24:07 +0530

    examples

--- jsonclient0.go
+++ jsonclient0.go
@@ -12,3 +12,16 @@ import (
 // currency service client (json)
 // sends request as curr1.CurrencyRequest
 // receives []curr1Curency
+
+func main() {
+var param string
+fmt.Print("Currency> ")
+_, err := fmt.Scanf("%s", &param)
+
+buf := new(bytes.Buffer)
+currRequest := &curr1.CurrencyRequest{Get: param}
+err = json.NewEncoder(buf).Encode(currRequest)
+if err != nil {
+   fmt.Println(err)
+   return
+}
