fdcfa38107e72110541ef44648dc51d05a24c1e8
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-05 12:44:09 +0530

    examples

--- jsonserv1.go
+++ jsonserv1.go
@@ -17,3 +17,21 @@ import (
 // curl-X POST -d '{"get":"Euro"}' http://localhost:4040/currency
 
 var currencies = curr1.Load("./data.csv")
+
+func currs(resp http.ResponseWriter, req *http.Request) {
+   var currRequest curr1.CurrencyRequest
+   dec := json.NewDecoder(req.Body)
+   if err := dec.Decode(&currRequest); err != nil {
+      resp.WriteHeader(http.StatusBadRequest)
+      fmt.Println(err)
+      return 
+   }
+   
+   result := curr1.Find(currencies, currRequest.Get)
+   enc := json.NewEncoder(resp)
+   if err := enc.Encode(&result); err != nil {
+      fmt.Println(err)
+      resp.WriteHeader(http.StatusInternalServerError)
+      return
+   }
+}
