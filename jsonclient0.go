96071ba2fb3e758bded35ab503f9d8e8ec9e514e
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-05 12:28:19 +0530

    examples

--- jsonclient0.go
+++ jsonclient0.go
@@ -14,28 +14,36 @@ import (
 // receives []curr1Curency
 
 func main() {
-var param string
-fmt.Print("Currency> ")
-_, err := fmt.Scanf("%s", &param)
-
-buf := new(bytes.Buffer)
-currRequest := &curr1.CurrencyRequest{Get: param}
-err = json.NewEncoder(buf).Encode(currRequest)
-if err != nil {
-   fmt.Println(err)
-   return
+   var param string
+   fmt.Print("Currency> ")
+   _, err := fmt.Scanf("%s", &param)
+   
+   buf := new(bytes.Buffer)
+   currRequest := &curr1.CurrencyRequest{Get: param}
+   err = json.NewEncoder(buf).Encode(currRequest)
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   
+   client := &http.Client{}
+   req, err := http.NewRequest("POST", "http://127.0.0.1:4040/currency", buf)
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   resp, err := client.Do(req)
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   defer resp.Body.Close()
+   
+   var currencies []curr1.Currency
+   err = json.NewDecoder(resp.Body).Decode(&currencies)
+   if err != nil {
+      fmt.Println(err)
+      return
+   }
+   fmt.Println(currencies)
 }
-
-client := &http.Client{}
-req, err := http.NewRequest("POST", "http://127.0.0.1:4040/currency", buf)
-if err != nil {
-   fmt.Println(err)
-   return
-}
-resp, err := client.Do(req)
-if err != nil {
-   fmt.Println(err)
-   return
-}
-defer resp.Body.Close()
-
