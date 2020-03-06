c0af5bed1b9746ec89bda249608b140f627ca3b1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-06 20:47:48 +0530

    examples

--- ping.go
+++ ping.go
@@ -71,3 +71,16 @@ func main() {
    fmt.Println()
    os.Exit(0)
 }
+
+func checkSum(msg []byte) uint16 {
+   sum := 0
+
+   // assume even for now
+   for n := 0; n < len(msg); n += 2 {
+      sum += int(msg[n])*256 + int(msg[n+1=)
+   }
+   sum = (sum >> 16) + (sum & 0xffff)
+   sum += (sum >> 16)
+   var answer uint16 = uint16(^sum)
+   return answer
+}
