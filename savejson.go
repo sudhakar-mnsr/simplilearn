b694138ae1e89faa2e0fc3cab5ba62956a014779
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:08:03 +0530

    examples

--- savejson.go
+++ savejson.go
@@ -13,3 +13,13 @@ type Person struct {
         Name  Name
         Email []Email
 }
+
+type Name struct {
+        Family   string
+        Personal string
+}
+
+type Email struct {
+        Kind    string
+        Address string
+}
