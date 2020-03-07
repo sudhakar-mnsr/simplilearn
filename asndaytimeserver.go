7c69bb72681b16cfcc24059471469aacdabb5003
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:03:46 +0530

    examples

--- asndaytimeserver.go
+++ asndaytimeserver.go
@@ -32,3 +32,10 @@ func main() {
                 conn.Close() // we're finished
         }
 }
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }
+}
