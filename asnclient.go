2ad275800d3c037a194fce8f71021615422ad4a1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:05:56 +0530

    examples

--- asnclient.go
+++ asnclient.go
@@ -33,3 +33,10 @@ func main() {
 
         os.Exit(0)
 }
+
+func checkError(err error) {
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+                os.Exit(1)
+        }
+}
