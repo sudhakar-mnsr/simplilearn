8bd8da65c2c92e8b8e53b0673afff32a77f30899
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:02:50 +0530

    examples

--- asndaytimeserver.go
+++ asndaytimeserver.go
@@ -9,3 +9,18 @@ import (
         "os"
         "time"
 )
+
+func main() {
+
+        service := ":1200"
+        tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+        checkError(err)
+
+        listener, err := net.ListenTCP("tcp", tcpAddr)
+        checkError(err)                                                                              
+
+        for {
+                conn, err := listener.Accept()
+                if err != nil {
+                        continue
+                }
