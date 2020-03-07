cf61422caa0341324228481e39771927f01dcb0f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:04:57 +0530

    examples

--- asnclient.go
+++ asnclient.go
@@ -11,3 +11,16 @@ import (
         "os"
         "time"
 )
+
+func main() {
+        if len(os.Args) != 2 {
+                fmt.Fprintf(os.Stderr, "Usage: %s host:port", os.Args[0])
+                os.Exit(1)
+        }
+        service := os.Args[1]
+
+        conn, err := net.Dial("tcp", service)                                                                              
+        checkError(err)
+
+        result, err := readFully(conn)
+        checkError(err)
