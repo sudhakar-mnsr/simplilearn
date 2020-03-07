f3c2d1350cb1f187eb7f25315ca8eeb4df1ff004
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:08:39 +0530

    examples

--- savejson.go
+++ savejson.go
@@ -23,3 +23,12 @@ type Email struct {
         Kind    string
         Address string
 }
+
+func main() {
+        person := Person{
+                Name: Name{Family: "Newmarch", Personal: "Jan"},
+                Email: []Email{Email{Kind: "home", Address: "jan@newmarch.name"},
+                        Email{Kind: "work", Address: "j.newmarch@boxhill.edu.au"}}}
+
+        saveJSON("person.json", person)
+}
