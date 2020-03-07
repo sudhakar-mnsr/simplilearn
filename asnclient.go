2b0af679bde568c80fd6c99a5d46ff461e6b96e1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-07 23:06:45 +0530

    examples

--- asnclient.go
+++ asnclient.go
@@ -46,3 +46,15 @@ func readFully(conn net.Conn) ([]byte, error) {
 
         result := bytes.NewBuffer(nil)
         var buf [512]byte
+        for {
+                n, err := conn.Read(buf[0:])
+                result.Write(buf[0:n])
+                if err != nil {
+                        if err == io.EOF {
+                                break
+                        }
+                        return nil, err
+                }
+        }
+        return result.Bytes(), nil
+}
