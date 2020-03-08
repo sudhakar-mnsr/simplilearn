c49095641e1045719a55a123a748713699eea5a3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-08 23:19:24 +0530

    examples

--- loadjson.go
+++ loadjson.go
@@ -23,3 +23,11 @@ type Email struct {
         Kind    string
         Address string
 }
+
+func (p Person) String() string {
+        s := p.Name.Personal + " " + p.Name.Family
+        for _, v := range p.Email {
+                s += "\n" + v.Kind + ": " + v.Address
+        }
+        return s
+}
