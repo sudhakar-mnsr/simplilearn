e08f37bbb72368cd90e2a9494d90a0d546e99f79
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-08 23:20:14 +0530

    examples

--- loadjson.go
+++ loadjson.go
@@ -38,3 +38,12 @@ func main() {
 
         fmt.Println("Person", person.String())
 }
+
+func loadJSON(fileName string, key interface{}) {
+        inFile, err := os.Open(fileName)
+        checkError(err)
+        decoder := json.NewDecoder(inFile)
+        err = decoder.Decode(key)
+        checkError(err)
+        inFile.Close()
+}
